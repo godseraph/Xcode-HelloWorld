e7509b8655e42866e5280c2ac819776701068946
godseraph <wangjch@sina.com>
2013-12-08 16:17:58 +0800

    Initial Commit

---
+++ HelloWorld/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Godseraph on 13-12-8.
+//  Copyright (c) 2013年 Godseraph. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
